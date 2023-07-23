e26bfa7d46074145144f90e00ca28d82f775d98e
mohammadarifsojib <67918517+mohammadarifsojib@users.noreply.github.com>
2023-07-23 22:48:21 +0600

    change var name and add autoplay

--- src/App.vue
+++ src/App.vue
@@ -3,55 +3,54 @@ import {ref} from "vue";
 
 const sliders = ref([
   {
-    name: 'Red car',
     path: 'https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
   },
   {
-    name: 'White car',
     path: 'https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=1024&q=60',
   },
   {
-    name: 'Blue car',
     path: 'https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
   },
   {
-    name: 'Blue car',
     path: 'https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60',
   },
 ])
 
-const sliderIndex = ref(0)
+const activeSlide = ref(0)
 
 function prevSlider() {
-  if (sliderIndex.value) {
-    sliderIndex.value--
+  if (activeSlide.value) {
+    activeSlide.value--
   } else {
-    sliderIndex.value = sliders.value.length - 1
+    activeSlide.value = sliders.value.length - 1
   }
 }
 
 function nextSlider() {
-  if (sliderIndex.value < sliders.value.length - 1) {
-    sliderIndex.value++
+  if (activeSlide.value < sliders.value.length - 1) {
+    activeSlide.value++
   } else {
-    sliderIndex.value = 0
+    activeSlide.value = 0
   }
 }
+
+function autoPlay(){
+  nextSlider()
+}
+
+setInterval(autoPlay, 3000)
 </script>
 
 <template>
   <main class="min-h-screen w-full py-20 px-5 flex items-center justify-center bg-gray-200">
     <!-- Slider wrapper -->
-    <div class="w-full relative">
+    <div class="w-7/12 relative">
 
       <!-- Single Slider -->
-      <div v-show="sliderIndex === index" v-for="(slider,index) in sliders"
+      <div v-show="activeSlide === index" v-for="(slider,index) in sliders"
            :key="index">
         <img :src="slider.path" alt="car"
              class="w-full h-96 object-cover rounded-lg shadow">
-        <p class="text-center mt-3 text-gray-200 text-xl uppercase">
-          {{ slider.name }}
-        </p>
       </div>
       <!-- Single Slider end -->
 
